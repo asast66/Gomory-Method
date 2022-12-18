8c2ef65e2b42c181791df230fd076e429b238976
aastashov <aastashov@atollis.com>
2022-12-18 18:43:57 +0300

    Updated template for SimplexMethodSolver and modified entry point

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from simplex_method import SimplexMethodSolver
+from simplex_method import SimplexMethodSolver, ProblemType, LimitConditionType
 
 
 def main():
-    solver = SimplexMethodSolver()
-    solver.solve()
+    target = ((3, 2), ProblemType.MAX)
+    limit_conditions = (
+        (2, 1, LimitConditionType.LE, 20),
+        (1, 3, LimitConditionType.LE, 27)
+    )
+    solver = SimplexMethodSolver(target=target,
+                                 limit_conditions=limit_conditions)
+    solution = solver.solve()
+    print(f'{solution = }')
 
 
 if __name__ == "__main__":
--- simplex_method.py
+++ simplex_method.py
@@ -1,7 +1,52 @@
+from enum import Enum
+from typing import List
+
+
+class ProblemType(Enum):
+    MAX = "max"
+    MIN = "min"
+
+
+class LimitConditionType(Enum):
+    LS = "ls"
+    LE = "le"
+    GT = "gt"
+    GE = "ge"
+
+
 class SimplexMethodSolver:
 
-    def __init__(self):
+    def __init__(self, target, limit_conditions):
+        self.__target = target
+        self.__limit_conditions = limit_conditions
+
+    def __to_canonical_form(self):
+        pass
+
+    def __build_simplex_table(self):
+        pass
+
+    def __evaluate_deltas(self) -> List[float]:
+        pass
+
+    def __check_deltas(self, deltas: List[float]) -> bool:
+        check = True
+        for delta in deltas:
+            if delta < 0:
+                check = False
+        return check
+
+    def __recalculate_simplex_table(self):
+        pass
+
+    def __get_answer_from_simplex_table(self):
         pass
 
     def solve(self):
-        pass
+        self.__to_canonical_form()
+        self.__build_simplex_table()
+        deltas = self.__evaluate_deltas()
+        while not self.__check_deltas(deltas):
+            self.__recalculate_simplex_table()
+            deltas = self.__evaluate_deltas()
+        return self.__get_answer_from_simplex_table()
